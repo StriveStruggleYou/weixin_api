27b86aaa5fe4aa3d9f8014202caf731271fd2d81
chendekai <chendekai@qipeng.com>
2018-06-07 11:55:50 +0800

    提交项目模块信息

---
+++ weixin-common/src/main/java/org/ssy/App.java
@@ -0,0 +1,13 @@
+package org.ssy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
